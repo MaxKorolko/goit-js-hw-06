5f3152eadb58551bc41b0cbbdfe514fd9af41570
MaxKorolko <m.v.korolko@gmail.com>
2022-05-30 11:00:21 +0300

    .

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,7 @@ const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
 
 inputEl.addEventListener("input", (event) => {
-  outputEl.textContent = event.currentTarget.value;
+  if (inputEl.value === "") {
+    outputEl.textContent = "Anonymous";
+  } else outputEl.textContent = event.currentTarget.value;
 });
